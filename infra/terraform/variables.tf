c544eb4e33ff61a27038cacee5318470077112d2
srikanth_karre2912 <srikanthkarre2912@gmail.com>
2025-10-18 00:27:10 +0530

    Update Terraform variables for EKS configuration
    Removed unused cluster_version variable and added subnet CIDR and availability zone variables to match main.tf configuration.

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -4,10 +4,10 @@ variable "cluster_name" {
   default     = "my-eks-cluster"
 }
 
-variable "cluster_version" {
-  description = "Kubernetes version for EKS cluster"
+variable "aws_region" {
+  description = "AWS region"
   type        = string
-  default     = "1.28"
+  default     = "us-east-"  # Changed to match your main.tf
 }
 
 variable "vpc_cidr" {
@@ -16,8 +16,58 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "aws_region" {
-  description = "AWS region"
+# Remove cluster_version since your main.tf doesn't use it
+# variable "cluster_version" {
+#   description = "Kubernetes version for EKS cluster"
+#   type        = string
+#   default     = "1.28"
+# }
+
+# Add these new variables that match your main.tf subnet configuration
+variable "subnet_cidr_a" {
+  description = "CIDR block for subnet A"
   type        = string
-  default     = "us-east-1"
+  default     = "10.0.1.0/24"
+}
+
+variable "subnet_cidr_b" {
+  description = "CIDR block for subnet B"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "availability_zone_a" {
+  description = "Availability zone for subnet A"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "availability_zone_b" {
+  description = "Availability zone for subnet B"
+  type        = string
+  default     = "us-east-1b"
+}
+
+variable "node_group_desired_size" {
+  description = "Desired number of nodes in node group"
+  type        = number
+  default     = 2
+}
+
+variable "node_group_min_size" {
+  description = "Minimum number of nodes in node group"
+  type        = number
+  default     = 1
+}
+
+variable "node_group_max_size" {
+  description = "Maximum number of nodes in node group"
+  type        = number
+  default     = 3
+}
+
+variable "node_instance_type" {
+  description = "Instance type for EKS nodes"
+  type        = string
+  default     = "t3.medium"
 }
