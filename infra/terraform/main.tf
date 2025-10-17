7695cb827b288dc47e46382d5e396493b501b794
srikanth_karre2912 <srikanthkarre2912@gmail.com>
2025-10-18 00:29:47 +0530

    Define AWS VPC, subnets, and EKS resources directly
    This change introduces new AWS VPC and subnet resources, along with an EKS cluster and node group configuration. The previous module definitions have been replaced with direct resource definitions for better clarity and control.

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,111 +1,47 @@
-# VPC
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
+provider "aws" {
+  region = var.aws_region
+}
 
-  name = "${var.cluster_name}-vpc"
-  cidr = var.vpc_cidr
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
 
-  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+resource "aws_subnet" "main_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr_a
+  availability_zone = var.availability_zone_a
+}
 
-  enable_nat_gateway   = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+resource "aws_subnet" "main_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr_b
+  availability_zone = var.availability_zone_b
+}
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_service_role.arn
+
+  vpc_config {
+    subnet_ids              = [aws_subnet.main_a.id, aws_subnet.main_b.id]
+    endpoint_private_access = true
+    endpoint_public_access  = true
   }
 }
 
-# EKS Cluster
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "my-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
 
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
+  subnet_ids = [aws_subnet.main_a.id, aws_subnet.main_b.id]
 
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.public_subnets
-
-  # EKS Managed Node Group
-  eks_managed_node_groups = {
-    default = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-
-      instance_types = ["t3.medium"]
-      capacity_type  = "ON_DEMAND"
-    }
+  scaling_config {
+    desired_size = var.node_group_desired_size
+    max_size     = var.node_group_max_size
+    min_size     = var.node_group_min_size
   }
 
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
-}
-
-# IAM Role for Service Account (for Load Balancer Controller)
-module "lb_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.0"
-
-  role_name = "${var.cluster_name}-lb-controller"
-
-  attach_load_balancer_controller_policy = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
-    }
-  }
-}
-
-# Data source for EKS cluster auth
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
-}
-
-# Kubernetes namespace resource
-resource "kubernetes_namespace" "app" {
-  metadata {
-    name = "devops-app"
-  }
-
-  depends_on = [module.eks]
-}
-
-# Output kubeconfig for CI/CD
-resource "local_file" "kubeconfig" {
-  content = <<YAML
-apiVersion: v1
-clusters:
-- cluster:
-    certificate-authority-data: ${module.eks.cluster_certificate_authority_data}
-    server: ${module.eks.cluster_endpoint}
-  name: ${module.eks.cluster_name}
-contexts:
-- context:
-    cluster: ${module.eks.cluster_name}
-    user: ${module.eks.cluster_name}
-  name: ${module.eks.cluster_name}
-current-context: ${module.eks.cluster_name}
-kind: Config
-preferences: {}
-users:
-- name: ${module.eks.cluster_name}
-  user:
-    token: ${data.aws_eks_cluster_auth.this.token}
-YAML
-
-  filename        = "${path.module}/kubeconfig"
-  file_permission = "0600"
-
-  depends_on = [module.eks]
+  instance_types = [var.node_instance_type]
+  capacity_type  = "ON_DEMAND"
 }
